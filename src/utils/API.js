9aa99a98d5bfc3dab46512e2c27b4bb72139fcbc
huynhpeter <huynhxpeter@gmail.com>
2019-10-19 12:57:58 -0400

    Add TODOs and changes from functional component to classs component

---
+++ src/utils/API.js
@@ -0,0 +1,9 @@
+import axios from 'axios'
+const BASEURL = 'https://www.omdbapi.com/?t='
+const APIKEY = '&apikey=trilogy'
+
+export default {
+  search: function(query) {
+    // TODO: Finish method to communicate with OMDB
+  }
+}
